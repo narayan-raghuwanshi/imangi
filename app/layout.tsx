1576df0ae73c137d14547a46f393d8731a9523e5
Narayan Raghuwanshi <narayan.raghu26@gmail.com>
2023-12-29 01:25:53 +0530

    fixing bug 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import Navbar from '@/components/Navbar'
 import Sidebar from '@/components/Sidebar'
+import { cn } from '@/lib/utils'
 
 const font = Inter({ subsets: ['latin'] });
 
@@ -20,7 +21,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body  className={font.className}>
+        <body  className={cn('min-h-screen bg-[#f5F5F5]',font.className)}>
           <Navbar />
           <Sidebar />
           <main className='flex justify-center items-center'>
