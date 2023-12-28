df8d3bed74862aad9683b849a2ab224c96c31cc6
Narayan Raghuwanshi <narayan.raghu26@gmail.com>
2023-12-29 01:01:32 +0530

    fixing bugs

---
+++ app/(auth)/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+const Error = () => {
+    return (
+        <h1 className="flex justify-center mt-30 text-xl font-semibold">Something went wrong..</h1>
+    );
+}
+
+export default Error;
